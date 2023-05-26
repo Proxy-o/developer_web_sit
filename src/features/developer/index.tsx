802fadaca08f4438315e2a4e015e134113340d67
OPERLY <96418976+Othmane-ait-taleb@users.noreply.github.com>
2023-05-26 09:52:56 +0100

    [wip] form

---
+++ src/features/developer/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import DevForm from "./devForm";
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <DevForm />
+    </>
+  );
+};
+
+export default Index;
