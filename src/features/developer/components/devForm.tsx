75b02896e29e1a624d661d1576956fcc82b3e9dd
othmane ait taleb <aittaleb108@gmail.com>
2023-05-27 10:08:24 +0100

    edit the dev form

--- src/features/developer/components/devForm.tsx
+++ src/features/developer/components/devForm.tsx
@@ -1,5 +1,7 @@
+import { Button } from "components/ui/button";
 import { Input } from "components/ui/input";
 import { Label } from "components/ui/label";
+import { Textarea } from "components/ui/textarea";
 import { useForm, type SubmitHandler } from "react-hook-form";
 
 interface IFormInput {
@@ -25,9 +27,9 @@ export default function DevForm() {
     <form
       // eslint-disable-next-line @typescript-eslint/no-misused-promises
       onSubmit={handleSubmit(onSubmit)}
-      className="m-auto max-w-4xl bg-green-600"
+      className="m-auto "
     >
-      <div className="flex gap-2">
+      <div className="mt-4 flex gap-2">
         <div className="w-full">
           <Label>
             <strong className="text-red-500">*</strong>First Name
@@ -61,7 +63,70 @@ export default function DevForm() {
           )}
         </div>
       </div>
-      <input type="submit" />
+      <div className="mt-4 w-full">
+        <Label>
+          <strong className="text-red-500">*</strong>Email
+        </Label>
+        <Input
+          placeholder="Email"
+          {...register("email", {
+            required: true,
+            maxLength: 20,
+            minLength: 2,
+            pattern: /^\S+@\S+$/i,
+          })}
+        />
+        {errors.email && (
+          <h1 className="m-2 ml-2 text-red-500">This field is required</h1>
+        )}
+      </div>
+      <div className="mt-4 w-full">
+        <Label>
+          <strong className="text-red-500">*</strong>Phone
+        </Label>
+        <Input
+          placeholder="Phone"
+          {...register("phone", {
+            required: true,
+            maxLength: 15,
+            minLength: 10,
+          })}
+        />
+        {errors.phone && (
+          <h1 className="m-2 ml-2 text-red-500">This field is required</h1>
+        )}
+      </div>
+      <div className="mt-4 w-full">
+        <Label>
+          <strong className="text-red-500">*</strong>Address
+        </Label>
+        <Input
+          placeholder="Address"
+          {...register("address", {
+            required: true,
+            maxLength: 100,
+            minLength: 2,
+          })}
+        />
+        {errors.address && (
+          <h1 className="m-2 ml-2 text-red-500">This field is required</h1>
+        )}
+      </div>
+      <div className="mt-4 w-full">
+        <Label>Portfolio</Label>
+        <Input
+          placeholder="Portfolio"
+          {...register("portfolio", {
+            maxLength: 100,
+          })}
+        />
+      </div>
+      <div className="mt-4 w-full">
+        <Label>Bio</Label>
+        <Textarea placeholder="Bio" {...register("bio")} />
+      </div>
+
+      <Button type="submit">Submit</Button>
     </form>
   );
 }
